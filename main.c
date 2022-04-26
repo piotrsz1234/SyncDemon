1d5b06f90549b9f5fde4386f59e811ee5f6bfc3a
piotrsz1234 <piotrsz1098@gmail.com>
2022-04-26 19:44:39 +0200

    Added logging functions body (#8)
    * Added logging functions body
    
    * Fix of an issue with random segmantation fault.
    
    Co-authored-by: Piotr Szuflicki <piotrszuflicki12@gmail.com>

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include "helper.h"
 #include <stdbool.h>
+#include <time.h>
 
 int main() {
-    UpdateDirectory("/mnt/c/Users/HP/Desktop/FileSync Test/origin", "/mnt/c/Users/HP/Desktop/FileSync Test/destination", true, 1);
+    
+    UpdateDirectory("/mnt/c/Users/HP/Desktop/FileSync Test/origin", "/mnt/c/Users/HP/Desktop/FileSync Test/destination", true, 1, 0);
+    long timestamp = time(0);
+    sleep(1);
+    UpdateDirectory("/mnt/c/Users/HP/Desktop/FileSync Test/origin", "/mnt/c/Users/HP/Desktop/FileSync Test/destination", true, 1, timestamp);
     return 0;
 }
