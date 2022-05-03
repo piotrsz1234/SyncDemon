41627dd4b185588076c2ac56127dddefa6773d86
piotrsz1234 <piotrsz1098@gmail.com>
2022-05-03 18:57:38 +0200

    Refactor of demon code and added code, which creates some big files
    * Some refactor
    
    * Fix some errors
    
    Co-authored-by: Piotr Szuflicki <piotrszuflicki12@gmail.com>

--- helper.h
+++ helper.h
@@ -17,12 +17,14 @@ bool ReadWriteCopyFile(char* originPath, char* fileName, char* destinationPath);
 
 List* GetFilesFromDirectory(char* directoryPath);
 
-bool UpdateDirectory(char* originDirectory, char* destinationDirectory, bool withDirectories, int minSizeForMMap, long lastTimestamp);
+bool UpdateDirectory(char* originDirectory, char* destinationDirectory, bool withDirectories, int minSizeForMMap);
 
-bool CreateAndSyncDirectory(char* originPath, char* destinationPath, char* directoryName, bool withDirectories, int minSizeForMMap, long lastTimestamp);
+bool CreateAndSyncDirectory(char* originPath, char* destinationPath, char* directoryName, bool withDirectories, int minSizeForMMap);
 
 bool MMapWriteCopyFile(char* originPath, char* fileName, char* destinationPath);
 
 long int GetFileSize(char* filePath);
+
+bool DirectoryExists(char* path);
 
 #endif
--- main.c
+++ main.c
@@ -1,12 +1,140 @@
 #include "helper.h"
 #include <stdbool.h>
 #include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#include <signal.h>
 
-int main() {
+static bool recursive = false;
+static int mmapMinSize = 1;
+static int sleepTime = 5;
+static char* originPath;
+static char* destinationPath;
+
+bool ParseArguments(int argc, char** argv) {
+    static struct option long_options[] = {
+        {"recursive", no_argument, 0, 'R'},
+        {"mmap",  required_argument, 0, 'm'},
+        {"sleepTime",  required_argument, 0, 's'},
+        {0, 0, 0, 0}
+    };
+    int index = 0;
+    while (true) {
+        int c = getopt_long (argc, argv, "Rm:s:",
+                       long_options, &index);
+
+        if(c == -1) {
+            break;
+        }
+
+        switch(c) {
+            case 'm':
+                mmapMinSize = atoi(optarg);
+            break;
+            case 's':
+                sleepTime = atoi(optarg);
+            break;
+             case 'R':
+                recursive = true;
+            break;
+        }
+    }
+    if (optind < argc)
+    {
+        if(optind + 2 > argc) {
+            printf("Destination path not specified!\n");
+            return false;
+        }
+        originPath = argv[optind++];
+        destinationPath = argv[optind];
+    } else {
+        printf("Origin path nor Destination paths specified!\n");
+        return false;
+    }
+    return true;
+}
+
+bool CheckPaths() {
+    bool result = true;
+    if(!DirectoryExists(originPath)) {
+        printf("Origin path specified does not exist or is not accessible\nPath: %s\n", originPath);
+        result = false;
+    }
+
+    if(!DirectoryExists(destinationPath)) {
+        printf("Destination path specified does not exist or is not accessible\nPath: %s\n", destinationPath);
+        result = false;
+    }
+
+    return result;
+}
+
+bool CanExecute(int argc, char** argv) {
+    if(!ParseArguments(argc, argv)) {
+        return false;
+    }
     
-    UpdateDirectory("/mnt/c/Users/HP/Desktop/FileSync Test/origin", "/mnt/c/Users/HP/Desktop/FileSync Test/destination", true, 1, 0);
-    long timestamp = time(0);
-    sleep(1);
-    UpdateDirectory("/mnt/c/Users/HP/Desktop/FileSync Test/origin", "/mnt/c/Users/HP/Desktop/FileSync Test/destination", true, 1, timestamp);
+    if(!CheckPaths()) {
+        return false;
+    }
+
+    return true;
+}
+
+void my_handler(int signum)
+{
+    if (signum == SIGUSR1){
+        ReportTrace("Demon has been awakened by SIGUSR1");
+        UpdateDirectory(originPath, destinationPath, recursive, mmapMinSize);
+    }
+}
+
+int main(int argc, char **argv) {
+    if(!CanExecute(argc, argv)) {
+        return -1;
+    }
+    
+    signal(SIGUSR1, my_handler);
+    
+    pid_t pid, sid;
+    pid = fork();
+
+    if (pid < 0) {
+            exit(EXIT_FAILURE);
+    }
+
+    if (pid > 0) {
+            exit(EXIT_SUCCESS);
+    }
+
+    umask(0);
+    sid = setsid();
+
+    if (sid < 0) {
+        exit(EXIT_FAILURE);
+    }
+    
+    if ((chdir("/")) < 0) {
+        exit(EXIT_FAILURE);
+    }
+    
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+    
+    while (true) {
+        ReportTrace("Demon has been awakened from slumber");
+        UpdateDirectory(originPath, destinationPath, recursive, mmapMinSize);
+        sleep(sleepTime * 60);
+    }
+
     return 0;
 }
